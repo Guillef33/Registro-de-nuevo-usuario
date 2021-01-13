263f3f7a356b6c677a6ae91fcce3a7dd4f1485d8
Guillef33 <guillef33@gmail.com>
2021-01-13 15:59:22 -0300

    function selection

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function check() {
-    document.getElementById('dia').checked = true;
-    document.getElementById('semana').checked = false;
-    document.getElementById('mes').checked = false;
-
-
-    document.getElementById('semana').checked = true;
-    document.getElementById('dia').checked = false;
-    document.getElementById('mes').checked = false;
-
-    document.getElementById('mes').checked = true;
-    document.getElementById('dia').checked = false;
-    document.getElementById('semana').checked = false;
+document.getElementById("dia").onclick = function(){
+	document.getElementById("semana").checked = false;
+	document.getElementById("mes").checked = false;
+}
+document.getElementById("semana").onclick = function(){
+	document.getElementById("dia").checked = false;
+	document.getElementById("mes").checked = false;
+}
+document.getElementById("mes").onclick = function(){
+	document.getElementById("dia").checked = false;
+	document.getElementById("semana").checked = false;
 }
 
 
